145f9b6a664fb174e92f28b625af2425948ac9c9
Ravi Netravali <ravinet@mit.edu>
2014-02-22 17:37:20 -0500

    Clarify comments

--- makebulkreply.cc
+++ makebulkreply.cc
@@ -20,7 +20,7 @@ int main( int argc, char *argv[] )
 {
     try {
         if ( argc != 3 ) {
-            throw Exception( "Usage", string( argv[0] ) + " folder_with_recorded_content hostname" );
+            throw Exception( "Usage", string( argv[0] ) + " folder_with_recorded_content hostname(eg. www.sears.com)" );
         }
         string directory( argv[ 1 ] );
         if( directory.back() != '/' ) {
@@ -29,13 +29,16 @@ int main( int argc, char *argv[] )
         string passed_header( argv[2] );
         vector< string > files;
         list_files( directory.c_str(), files );
+
+        /* first write number of request/response pairs to the bulk response */
         uint32_t num_files = files.size();
         FileDescriptor bulkreply = SystemCall( "open", open( "bulkreply.proto", O_WRONLY | O_CREAT, 00700 ) );
         SystemCall( "write", write( bulkreply.num(), &num_files, 4 ) );
-        unsigned int i;
 
+        unsigned int i;
         string first_response;
-        /* first put GET / response to original host and remove this from list of files */
+
+        /* first put GET / response to original host and remove this from list of files (do this since it should be first one listed) */
         for ( i = 0; i < num_files; i++ ) { // iterate through recorded files and find initial request protobuf
             FileDescriptor fd = SystemCall( "open", open( files[i].c_str(), O_RDONLY ) );
             HTTP_Record::reqrespair current_record;
@@ -43,17 +46,17 @@ int main( int argc, char *argv[] )
 
             HTTP_Record::http_message current_req;
             current_req = current_record.req();
-            if (current_req.first_line() == "GET / HTTP/1.1\r\n" ) {
+            if (current_req.first_line() == "GET / HTTP/1.1\r\n" ) { /* could be first request */
                 string host_header;
-                /* iterate through headers and check if host matches given host */
+                /* iterate through headers and check if host matches given host (since later requests to other servers can be GET /) */
                 for ( int j = 0; j < current_record.req().headers_size(); j++ ) {
                     HTTPHeader current_header( current_record.req().headers(j) );
-                    if ( HTTPMessage::equivalent_strings( current_header.key(), "HOST" ) ) {
+                    if ( HTTPMessage::equivalent_strings( current_header.key(), "HOST" ) ) { /* store host header value */
                         host_header = current_header.value();
                            break;
                     }
                 }
-                if ( host_header.substr(0, host_header.find("\r\n")) == passed_header ) {
+                if ( host_header.substr(0, host_header.find("\r\n")) == passed_header ) { /* host header value matches passed value so this is correct first request */
                     /* write request and size to bulk response */
                     string current_req;
                     current_record.req().SerializeToString( &current_req );
@@ -67,11 +70,12 @@ int main( int argc, char *argv[] )
                 }
             }
         }
-        /* remove file from list of files */
+        /* remove file for first request/response from list of files */
         files.erase( files.begin() + i );
         num_files = files.size();
 
-        for ( i = 0; i < num_files; i++ ) { /* iterate through recorded files and for each request, append size and request to bulkreply */
+        /* iterate through recorded files and for each request, append size and request to bulkreply */
+        for ( i = 0; i < num_files; i++ ) {
             FileDescriptor fd = SystemCall( "open", open( files[i].c_str(), O_RDONLY ) );
             HTTP_Record::reqrespair current_record;
             current_record.ParseFromFileDescriptor( fd.num() );
@@ -84,12 +88,13 @@ int main( int argc, char *argv[] )
             current_record.req().SerializeToFileDescriptor( bulkreply.num() );
         }
 
-        /* add first response */
+        /* add first response (prepended with its size) to bulk response */
         uint32_t first_res_size = first_response.size();
         SystemCall( "write", write( bulkreply.num(), &first_res_size, 4 ) );
         bulkreply.write( first_response );
 
-        for ( i = 0; i < num_files; i++ ) { /* iterate through recorded files and for each response, append size and response to bulkreply */
+        /* iterate through recorded files and for each response, append size and response to bulkreply */
+        for ( i = 0; i < num_files; i++ ) {
             FileDescriptor fd = SystemCall( "open", open( files[i].c_str(), O_RDONLY ) );
             HTTP_Record::reqrespair current_record;
             current_record.ParseFromFileDescriptor( fd.num() );
